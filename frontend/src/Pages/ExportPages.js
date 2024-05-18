23955fc5c598fe253a4ddbed845f88fd79b81356
unawarexi <unawarexi@gmail.com>
2024-05-18 01:26:35 +0100

    main export file for jsx files  to objects imports

---
+++ frontend/src/Pages/ExportPages.js
@@ -0,0 +1,3 @@
+export { default as LandingPage } from "./LandingPage";
+export { default as Customers } from "./Customers";
+export { default as CreateCustomer } from "./CreateCustomer";
--- frontend/src/components/ExportComponents.js
+++ frontend/src/components/ExportComponents.js
@@ -3,3 +3,7 @@ export { default as Footer } from "./Footer.jsx";
 
 // Admin
 export { default as UserDetail } from "./UserDetail.jsx";
+export { default as CustomerDetail } from "./CustomerDetail.jsx";
+
+//forms and creating customer
+export { default as CustomerForm } from "./CustomerForm.jsx";
