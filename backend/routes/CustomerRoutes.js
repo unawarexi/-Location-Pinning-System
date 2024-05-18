52286e7b438fb5a3df662040ac02a59a700985d4
unawarexi <unawarexi@gmail.com>
2024-05-18 03:33:41 +0100

    api routes for customer crud calls

--- backend/routes/CustomerRoutes.js
+++ backend/routes/CustomerRoutes.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const router = express.Router();
-// const auth = require("../middleware/CustomerAuth");
-const customerController = require("../controllers/CustomerController");
+const {
+  createCustomer,
+  getAllCustomers,
+  getCustomerById,
+  updateCustomer,
+  deleteCustomer,
+} = require("../controllers/CustomerController");
+const UserAuth = require("../middleware/UserAuth");
 
-router.post("/", customerController.createCustomer);
+// Route for creating a new customer
+router.post("/", UserAuth, createCustomer);
+
+// Route for getting all customers
+router.get("/", UserAuth, getAllCustomers);
+
+// Route for getting a single customer by ID
+router.get("/:id", UserAuth, getCustomerById);
+
+// Route for updating a customer
+router.put("/:id", UserAuth, updateCustomer);
+
+// Route for deleting a customer
+router.delete("/:id", UserAuth, deleteCustomer);
 
 module.exports = router;
