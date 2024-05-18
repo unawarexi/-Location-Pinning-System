7a33f5a71ee65c5c304efc3d43572a848a187a66
unawarexi <unawarexi@gmail.com>
2024-05-18 01:20:37 +0100

    providing global state data using context Api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from "./Pages/LandingPage";
+import { Customers, CreateCustomer, LandingPage } from "./Pages/ExportPages";
 import MainLayout from "./layout/MainLayout";
+import { GetStarted } from "./container/ExportContainer";
+import { AuthProvider } from "./context/AuthContext";
 
 const App = () => {
   return (
-    <div className="max-w-7xl mx-auto">
-      <>
-        <BrowserRouter>
-          <MainLayout>
-            <Routes>
-              <Route path="/" element={<LandingPage />} />
-            </Routes>
-          </MainLayout>
-        </BrowserRouter>
-      </>
-    </div>
+    <AuthProvider>
+      <div className="max-w-7xl mx-auto">
+        <>
+          <BrowserRouter>
+            <MainLayout>
+              <Routes>
+                <Route path="/auth" element={<GetStarted />} />
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/customers" element={<Customers />} />
+                <Route path="/create" element={<CreateCustomer />} />
+              </Routes>
+            </MainLayout>
+          </BrowserRouter>
+        </>
+      </div>
+    </AuthProvider>
   );
 };
 
