fb1f21a6db88a20aaf9567ab30bd47516b0f79a4
unawarexi <unawarexi@gmail.com>
2024-05-18 01:33:23 +0100

    Updated

--- frontend/src/Pages/LandingPage.jsx
+++ frontend/src/Pages/LandingPage.jsx
@@ -20,10 +20,10 @@ const LandingPage = () => {
             </p>
             <div className="flex justify-center">
               <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                Button
+                create customer
               </button>
               <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
-                Button
+                view customer details
               </button>
             </div>
           </div>
