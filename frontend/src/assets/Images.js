fd0ec201257f7475db2a82c1993a7b291fe740ca
unawarexi <unawarexi@gmail.com>
2024-05-19 01:04:55 +0100

    Customer Location Pinning System

--- frontend/src/assets/Images.js
+++ frontend/src/assets/Images.js
@@ -1,1 +1,5 @@
 export { default as HeroImage } from "./images/house-location.png";
+
+export { default as customerImage } from "./images/customerimg.png";
+export { default as updateImage } from "./images/updateimg.png";
+export { default as error } from "./images/error.png";
