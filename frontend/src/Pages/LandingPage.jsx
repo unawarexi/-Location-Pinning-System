e935ab3f98fbe49df41d3575d6571dd3d9d58afe
unawarexi <unawarexi@gmail.com>
2024-05-19 01:11:26 +0100

    branding and cleaning up

--- frontend/src/Pages/LandingPage.jsx
+++ frontend/src/Pages/LandingPage.jsx
@@ -9,15 +9,15 @@ const LandingPage = () => {
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-              Before they sold out
+              Lets get to them before
               <br className="hidden lg:inline-block" />
-              readymade gluten
+              someone else does!!!
             </h1>
             <p className="mb-8 leading-relaxed">
-              Copper mug try-hard pitchfork pour-over freegan heirloom neutra
-              air plant cold-pressed tacos poke beard tote bag. Heirloom echo
-              park mlkshk tote bag selvage hot chicken authentic tumeric
-              truffaut hexagon try-hard chambray.
+              Reach out to your customers with personalized services and offers
+              that they won't find anywhere else. your commitment to quality and
+              customer satisfaction ensures that you'll always stay ahead of the
+              competition.
             </p>
             <div className="flex justify-center">
               <Link
