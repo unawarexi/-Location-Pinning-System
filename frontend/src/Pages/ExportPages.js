3b39b9e19695705ff577784899a0d37acc0b1178
unawarexi <unawarexi@gmail.com>
2024-05-19 01:07:10 +0100

    Customer Location Pining System

--- frontend/src/Pages/ExportPages.js
+++ frontend/src/Pages/ExportPages.js
@@ -1,3 +1,5 @@
 export { default as LandingPage } from "./LandingPage";
 export { default as Customers } from "./Customers";
 export { default as CreateCustomer } from "./CreateCustomer";
+export { default as CustomerDetails } from "./CustomerDetails";
+export { default as UpdateCustomer } from "./UpdateCustomer";
--- frontend/src/components/ExportComponents.js
+++ frontend/src/components/ExportComponents.js
@@ -1,9 +1,5 @@
 export { default as Header } from "./Header.jsx";
 export { default as Footer } from "./Footer.jsx";
 
-// Admin
-export { default as UserDetail } from "./UserDetail.jsx";
-export { default as CustomerDetail } from "./CustomerDetail.jsx";
-
 //forms and creating customer
 export { default as CustomerForm } from "./CustomerForm.jsx";
