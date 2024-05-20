547cff0bc300004e3ff4e8d4ef566bc47a26289d
unawarexi <unawarexi@gmail.com>
2024-05-20 18:49:20 +0100

    Updated

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,15 +1,15 @@
-const express = require("express");
-const { registerUser, loginUser } = require("../controllers/UserController");
-const UserAuth = require("../middleware/UserAuth");
-const router = express.Router();
+// const express = require("express");
+// // const { registerUser, loginUser } = require("../controllers/UserController");
+// // const UserAuth = require("../middleware/UserAuth");
+// // const router = express.Router();
 
-// Route for user registration
-router.post("/register", registerUser);
+// // // Route for user registration
+// // router.post("/register", registerUser);
 
-// Route for user login
-router.post("/login", loginUser);
+// // // Route for user login
+// // router.post("/login", loginUser);
 
-// Applied the middleware to required routes
-router.use(UserAuth);
+// // // Applied the middleware to required routes
+// // router.use(UserAuth);
 
-module.exports = router;
+// module.exports = router;
