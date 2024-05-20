ec2ab659a9893ddc0586fbe6884022709090cc56
unawarexi <unawarexi@gmail.com>
2024-05-20 12:31:35 +0100

    FIXES

--- backend/index.js
+++ backend/index.js
@@ -7,24 +7,29 @@ const userRoutes = require("./routes/userRoutes");
 const customerRoutes = require("./routes/CustomerRoutes");
 
 const app = express();
-
-app.use(
-  cors({
-    origin: ["https://system-location.vercel.app"],
-    methods: ["post", "get", "put", "update", "delete"],
-    credentials: true,
-  })
-);
 app.use(express.json());
 
 //Dotenv configuration
 require("dotenv").config();
 
-const PORT = process.env.PORT || 5000;
+app.use(
+  cors({
+    origin: ["https://system-location.vercel.app"],
+    methods: ["POST", "GET", "PUT", "UPDATE", "DELETE"],
+    credentials: true,
+  })
+);
+
+//  controllers
+app.get("/", (req, res) => {
+  res.send("server is running..");
+});
 
 //Working with routes
 app.use("/api/users", userRoutes);
 app.use("/api/customers", customerRoutes);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server started at port ${PORT}`);
