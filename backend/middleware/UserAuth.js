8ff2a6cf13c77cd04e5aad7e3daf9bc9b7db88ed
unawarexi <unawarexi@gmail.com>
2024-05-21 04:53:40 +0100

    updated

--- backend/middleware/UserAuth.js
+++ backend/middleware/UserAuth.js
@@ -1,19 +1,18 @@
 const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
-  const authHeader = req.header("Authorization");
+  const token = req.cookies.token;
 
-  if (!authHeader) {
+  if (!token) {
     return res.status(401).json({ message: "No token, authorization denied" });
   }
-
-  const token = authHeader.replace("Bearer ", "");
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded.id;
     next();
   } catch (error) {
+    res.clearCookie("token"); // Clear invalid token from cookies
     res.status(401).json({ message: "Token is not valid", error });
   }
 };
